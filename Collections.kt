bbcfc9621fbe9cecd38988e6502369089491b03a
zain-asif-96 <zain.raza199699@gmail.com>
2025-03-14 13:49:54 +0500

    chore: list collection examples added to the file

--- Collections.kt
+++ Collections.kt
@@ -22,4 +22,49 @@ fun main() {
     allSubjects[3] = "Physics";
     
     println(allSubjects[3])
+
+
+
+    // 2. List as MutableList
+    /** A list is a collection of items that are ordered and can be accessed by their index. 
+     * In Kotlin, you can create a list using the listOf() function. 
+     * The listOf() function creates an immutable list, which means that you cannot add or remove items from the list after it has been created.
+     * A list is an ordered, resizable collection, typically implemented as a resizable array. When the array is filled to capacity and you try to insert a new element, the array is copied to a new bigger array.
+     * List is an interface that defines properties and methods related to a read-only ordered collection of items.
+     * MutableList extends the List interface by defining methods to modify a list, such as adding and removing elements.
+    **/
+    
+    val attendees = mutableListOf<String>("Ali", "Ahmed", "Asad")
+    
+    
+    //Add elements to the list
+    attendees.add("Ahsan")
+    attendees.add(3,"Adeel")
+    attendees.add("Aamir")
+    
+    //Remove elements from the list
+    attendees.remove("Ali")
+    attendees.removeAt(3)
+    
+    
+    //Length of list
+    println(attendees.size)
+    
+    //Access the elements
+    println(attendees[3])
+    println(attendees.get(3))
+    
+    //Find index of element
+    println(attendees.indexOf("Adeel"))
+    
+    
+    //Show all the elements by using for loop
+    for(attendee in attendees) {
+        println(attendee)
+    }
+    
+    
+    //Check if elements present in the list
+    println("Ali" in attendees)
+    println(attendees.contains("Ahmed"))
 }
